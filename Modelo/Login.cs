e861f5f862d2e20792e6d714cf6380093e1aca9e
Elejota <luiscarloslealjimenez@hotmail.com>
2023-08-25 09:43:46 -0500

    Verion entrada y salida ok, Login en process

---
+++ Modelo/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Login
+    {
+        public string Documento { get; set; }
+        public string Contraseña { get; set; }
+        public string ContraseñaNueva { get; set; }
+
+    }
+}
--- Servicios/RepositorioLogin.cs
+++ Servicios/RepositorioLogin.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Modelo;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,33 @@ namespace Servicios
 {
     public class RepositorioLogin
     {
+        public DataTable ConsultarContraseñaPorDocumento(Login login)
+        {
+            DataTable tabla = new DataTable();
+            SqlConnection sqlCon = new SqlConnection();
+            SqlDataReader resultado;
+            try
+            {
+                sqlCon = RepositorioConexion.GetInstancia().CrearConexionLocal();
+                string cadena = ("SELECT Documento,NombreEmpleado,ApellidoEmpleado,ContraseñaEmpleado FROM T_Empleados WHERE Estado=1");
+                SqlCommand comando = new SqlCommand(cadena, sqlCon);
+                sqlCon.Open();
+                resultado = comando.ExecuteReader();
+                tabla.Load(resultado);
+                return tabla;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open) sqlCon.Close();
+            }
+
+        }
 
     }
 }
